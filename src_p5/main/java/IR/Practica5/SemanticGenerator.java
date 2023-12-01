45c5c191afd54590def2ccc953805bd749ef32a8
pifica2002 <46220743+pifica2002@users.noreply.github.com>
2023-12-01 21:20:08 +0100

    Ulitma version discord viernes

--- src_p5/main/java/IR/Practica5/SemanticGenerator.java
+++ src_p5/main/java/IR/Practica5/SemanticGenerator.java
@@ -3,6 +3,7 @@ package IR.Practica5;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.vocabulary.RDFS;
 import org.apache.jena.vocabulary.VCARD;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -41,7 +42,7 @@ public class SemanticGenerator {
         // args[3] = <docsPath>
 
         if (!args[0].equals("-rdf") || !args[2].equals("-docs")) {
-            System.out.println("Argumentos incorrectos. Uso: SemanticSearcher -rdf <rdfPath> -docs <docsPath>");
+            System.out.println("Argumentos incorrectos. Uso: SemanticGenerator -rdf <rdfPath> -docs <docsPath>");
             System.exit(1);
         }
 
@@ -60,7 +61,7 @@ public class SemanticGenerator {
      */
     private static void comprobarParametros(int numParametros){
         if (numParametros != 4) {
-            System.out.println("Número incorrecto de parámetros.\nUso: SemanticSearcher -rdf <rdfPath> -docs <docsPath>");
+            System.out.println("Número incorrecto de parámetros.\nUso: SemanticGenerator -rdf <rdfPath> -docs <docsPath>");
             System.exit(1);
         }
     }
@@ -133,22 +134,27 @@ public class SemanticGenerator {
 
             // Se obtiene la lista de nodos con nombre "dc:creator"
             org.w3c.dom.NodeList listaCreator = raiz.getElementsByTagName("dc:creator");
+            // Se crea una lista para almacenar los valores de los creadores
+            List<String> creators = new ArrayList<>();
             // Se recorre la lista de nodos
             for (int i = 0; i < listaCreator.getLength(); i++) {
                 // Se obtiene el nodo actual
                 org.w3c.dom.Node nodo = listaCreator.item(i);
-                // Se obtiene el texto que contiene el nodo actual
-                creator = nodo.getTextContent();
+                // Se obtiene el texto que contiene el nodo actual y se agrega a la lista
+                creators.add(nodo.getTextContent());
             }
 
             // Se obtiene la lista de nodos con nombre "dc:contributor"
             org.w3c.dom.NodeList listaContributor = raiz.getElementsByTagName("dc:contributor");
+            // Se crea una lista para almacenar los valores de los contribuyentes
+            List<String> contributors = new ArrayList<>();
             // Se recorre la lista de nodos
             for (int i = 0; i < listaContributor.getLength(); i++) {
                 // Se obtiene el nodo actual
                 org.w3c.dom.Node nodo = listaContributor.item(i);
-                // Se obtiene el texto que contiene el nodo actual
-                contributor = nodo.getTextContent();
+                
+                // Se obtiene el texto que contiene el nodo actual y se agrega a la lista
+                contributors.add(nodo.getTextContent());
             }
 
             // Se obtiene la lista de nodos con nombre "dc:publisher"
@@ -225,14 +231,25 @@ public class SemanticGenerator {
             date = date.replaceAll("\\r\\n|\\r|\\n", "_");
             language = language.replaceAll("\\r\\n|\\r|\\n", "_");
             identifier = identifier.replaceAll("\\r\\n|\\r|\\n", "_");
-            creator = creator.replaceAll("\\r\\n|\\r|\\n", "_");
-            contributor = contributor.replaceAll("\\r\\n|\\r|\\n", "_");
+            //creator = creator.replaceAll("\\r\\n|\\r|\\n", "_");
+            //contributor = contributor.replaceAll("\\r\\n|\\r|\\n", "_");
             publisher = publisher.replaceAll("\\r\\n|\\r|\\n", "_");
             relation = relation.replaceAll("\\r\\n|\\r|\\n", "_");
             rights = rights.replaceAll("\\r\\n|\\r|\\n", "_");
             subject = subject.replaceAll("\\r\\n|\\r|\\n", "_");
             type = type.replaceAll("\\r\\n|\\r|\\n", "_");
+
+            // Se eliminan los saltos de linea y espacios por _ en contributor
+            for (int i = 0; i < contributors.size(); i++) {
+                contributors.set(i, contributors.get(i).replaceAll("\\r\\n|\\r|\\n", "_"));
+            }
+
+            // Se eliminan los saltos de linea y espacios por _ en creator
+            for (int i = 0; i < creators.size(); i++) {
+                creators.set(i, creators.get(i).replaceAll("\\r\\n|\\r|\\n", "_"));
+            }
             
+
             // Se eliminan los saltos de line y espacios en subject
             for (int i = 0; i < subjects.size(); i++) {
                 subjects.set(i, subjects.get(i).replaceAll("\\r\\n|\\r|\\n", "_"));
@@ -241,8 +258,8 @@ public class SemanticGenerator {
             
 
             generarRDFModelo(file, directorio_docs, directorio_rdf, 
-                            title, description, date, language, creator, 
-                            contributor, relation, rights, type, subjects);
+                            title, description, date, language, creators, 
+                            contributors, relation, rights, type, subjects,publisher);
 
         } catch (ParserConfigurationException e) {
             // Handle ParserConfigurationException
@@ -273,95 +290,82 @@ public class SemanticGenerator {
         }
     }
 
-    public static void generarRDFModelo(File file,
-                                        String directorio_docs, String directorio_rdf, String title, String description, String date, String language,
-                                        String creator, String contributor, String relation, String rights, String type, List<String> subjects) {
+   
+    public static void generateRDFModel(File file, String outputDirectory, String title, String description, String date, String language,
+                                        List<String> creators, List<String> contributors, String relation, String rights, String type, List<String> subjects, String publisher) {
 
-        // Se crea el modelo RDF
+        // Create an in-memory RDF model
         Model modelRDF = ModelFactory.createDefaultModel();
 
-        // Se crean las propiedades (atributos del documento)
-        Property titleProperty = modelRDF.createProperty(DC.title.toString());
-        Property languageProperty = modelRDF.createProperty(DC.language.toString());
-        Property descriptionProperty = modelRDF.createProperty(DC.description.toString());
-        Property dateProperty = modelRDF.createProperty(DC.date.toString());
-        Property subjectProperty = modelRDF.createProperty(DC.subject.toString());
-        Property publisherProperty = modelRDF.createProperty("http://xmlns.com/foaf/0.1/publisher"); // dc:contenido -> dc:publusher del xml
-        Property contributorProperty = modelRDF.createProperty(DC.contributor.toString());
-        
-        Property rightsProperty = modelRDF.createProperty(DC.rights.toString());
-        Property relationProperty = modelRDF.createProperty(DC.relation.toString());
-        
-        
-        Property typeProperty = modelRDF.createProperty(DC.type.toString());
-        Property nameProperty = modelRDF.createProperty("http://xmlns.com/foaf/0.1/name");
-        
-        // Se crean las clases
-        Resource personClass = modelRDF.createResource("http://xmlns.com/foaf/0.1/Person");
-        Resource urlClass = modelRDF.createResource("http://xmlns.com/foaf/0.1/Creator");
-        Resource subjecClass = modelRDF.createResource("http://xmlns.com/foaf/0.1/Subject");
-        Resource publisherClass = modelRDF.createResource("http://xmlns.com/foaf/0.1/Publisher");
+        // Define the DC properties
+        Property titleProperty = DC.title;
+        Property languageProperty = DC.language;
+        Property descriptionProperty = DC.description;
+        Property dateProperty = DC.date;
 
-        // Se crea la instancia de documento
+        Property subjectProperty = DC.subject;
+        Property nameProperty = modelRDF.createProperty("http://xmlns.com/foaf/0.1/name");
+        Property publisherProperty = modelRDF.createProperty("http://xmlns.com/foaf/0.1/publisher");
+        Property rightsProperty = DC.rights;
+        Property relationProperty = DC.relation;
+
+        // Create the document instance
         Resource documentInstance = modelRDF.createResource()
                 .addProperty(titleProperty, title)
                 .addProperty(languageProperty, language)
                 .addProperty(descriptionProperty, description)
                 .addProperty(dateProperty, date);
-                
 
-        // Se crean las instancias de subject
-        for (int i = 0; i < subjects.size(); i++) {
+        // Create the publisher class and relate it to the document instance
+        Resource publisherClass = modelRDF.createResource()
+                .addProperty(nameProperty, publisher);
+        documentInstance.addProperty(publisherProperty, publisherClass);
+
+        // Create subject instances and relate them to the document instance
+        for (String subject : subjects) {
             Resource subjectInstance = modelRDF.createResource()
-                    .addProperty(subjectProperty, subjects.get(i));
+                    .addProperty(nameProperty, subject);
+            documentInstance.addProperty(subjectProperty, subjectInstance);
         }
 
-        // Se crea la instancia de persona (creador)
-        Resource personaInstance = modelRDF.createResource()
-                .addProperty(nameProperty, creator);
-                //.addProperty(nameProperty, contributor);
-
-        // Se crea la instancia de URL (relation)
-        Resource urlInstance = modelRDF.createResource()
-                .addProperty(relationProperty, relation)
-                .addProperty(rightsProperty, rights);
-
-        // Se añaden las relaciones
-        documentInstance.addProperty(contributorProperty, contributor);
-        //documentInstance.addProperty(relationProperty, urlInstance);
-        documentInstance.addProperty(rightsProperty, rights);
-        documentInstance.addProperty(typeProperty, type);
-
-        // Se añade la relación entre documento y creador
-        //documentInstance.addProperty(FOAF. + "creator", creatorInstance);
-
-
-        // // Obtener el nombre del archivo sin extensión usando Apache Commons IO
-        //System.out.println("Nombreficheri: "+file.getName());
-        int posicionPunto = file.getName().lastIndexOf(".");
-        String nombreSinExtension = "";
-        String nombreFicheroCrear = "";
-        if (posicionPunto != -1) {
-            // Extraer el nombre del fichero sin la extensión
-            nombreSinExtension = file.getName().substring(0, posicionPunto);
-            //System.out.println("Nombre del fichero sin extensión: " + nombreSinExtension);
-
-            // Almacenar el resultado en otra variable
-            nombreFicheroCrear = nombreSinExtension + ".rdf";
-            //System.out.println("Nombre para crear: " + nombreFicheroCrear );
-            // escribe el modelo en un archivo
-            
-        } else {
-            System.out.println("El fichero no tiene extensión.");
+        // Create creator instances and relate them to the document instance
+        for (String creator : creators) {
+            Resource creatorInstance = modelRDF.createResource()
+                    .addProperty(nameProperty, creator);
+            documentInstance.addProperty(DC.creator, creatorInstance);
         }
-        String nombre_fichero_rdf = nombreFicheroCrear;
-        // // Crear el archivo RDF de salida con el mismo nombre pero con extensión ".rdf"
-        // String outputFileName = nameWithoutExtension + ".rdf";
-        // String outputFilePath = new File(outputDirectory, outputFileName).getAbsolutePath();
 
-        //System.out.println("FICHERO: " + nombre_fichero_rdf);
-        // Se imprime el modelo RDF
-        SemanticGenerator.generarArchivoRdf(directorio_rdf, nombre_fichero_rdf, modelRDF);
+        // Create contributor instances and relate them to the document instance
+        for (String contributor : contributors) {
+            Resource contributorInstance = modelRDF.createResource()
+                    .addProperty(nameProperty, contributor);
+            documentInstance.addProperty(DC.contributor, contributorInstance);
+        }
+
+        // Create URL instances for rights and relation and relate them to the document instance
+        Resource rightsInstance = modelRDF.createResource(rights);
+        Resource relationInstance = modelRDF.createResource(relation);
+        documentInstance.addProperty(rightsProperty, rightsInstance);
+        documentInstance.addProperty(relationProperty, relationInstance);
+
+        // Create subclasses TFG and TESIS and relate them to the document instance
+        
+        if(type == "TFG"){
+            Resource class_TFG = modelRDF.createResource()
+                .addProperty(RDFS.subClassOf, documentInstance);
+        }else if(type == "TESIS"){
+            Resource class_TESIS = modelRDF.createResource()
+                .addProperty(RDFS.subClassOf, documentInstance);
+        }
+        
+        // Extract the name of the file without extension
+        String nameWithoutExtension = file.getName().replaceFirst("[.][^.]+$", "");
+
+        // Create the RDF file name
+        String outputFileName = nameWithoutExtension + ".rdf";
+
+        // Generate the RDF file
+        SemanticGenerator.generateRDFFile(outputDirectory, outputFileName, modelRDF);
     }
     
 
@@ -372,15 +376,17 @@ public class SemanticGenerator {
         File directory = new File(directorio_docs);
         //System.out.println(directory);
         File[] files = directory.listFiles();
-        
+        Integer i = 0;
         if (files != null) {
             //System.out.println("Documentos XML encontrados en el directorio:");
             for (File file : files) {
                 if (file.isFile() && file.getName().toLowerCase().endsWith(".xml")) {
-
-                        //System.out.println(file.getName()); // <- imprime el nombre del fichero xml 
+                    if(i == 0) {
+                        System.out.println(file.getName()); // <- imprime el nombre del fichero xml
                         SemanticGenerator.parserXML(file, directorio_docs, directorio_rdf);
 
+                    }
+                    i = 1;
 
                 }
             }
--- src_p5/main/java/IR/Practica5/SemanticSearcher.java
+++ src_p5/main/java/IR/Practica5/SemanticSearcher.java
@@ -1,64 +1,105 @@
 package IR.Practica5;
+
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.tdb.TDBFactory;
+
 import java.io.File;
 
-
 public class SemanticSearcher {
-    
 
-    public static void main(String[] args){
+    /**
+     * Main principal
+     */
 
-        if(args.length < 6){
-            System.out.println("Uso: java SemanticSearcher -rdf <rdfPath> -infoNeeds <infoNeedsFile> -output <resultsFile>");
+    public static void main(String[] args) {
+        // Se comprueba que el número de parámetros es correcto
+        SemanticSearcher.comprobarParametros(args.length);
+
+        // args[0] = -rdf
+        // args[1] = <rdfPath>
+        // args[2] = -infoNeeds
+        // args[3] = <infoNeedsFile>
+        // args[4] = -output
+        // args[5] = <resultsFile>
+
+        if (!args[0].equals("-rdf") || !args[2].equals("-infoNeeds") || !args[4].equals("-output")) {
+            System.out.println("Argumentos incorrectos. Uso: SemanticSearcher -rdf <rdfPath> -infoNeeds <infoNeedsFile> -output <resultsFile>");
             System.exit(1);
         }
 
-        // Si los parámetros son correctos, se obtienen los valores de los mismos
-        String rdfPath = null;
-        String infoNeedsFile = null;
-        String resultsFile = null;
 
-        for (int i = 0; i < args.length; i++) {
-            if ("-rdf".equals(args[i]) && i + 1 < args.length) {
-                rdfPath = args[i + 1];
+        // Specify the directory for the TDB dataset
+        String datasetDir = "tdb_dataset";
 
-            } else if ("-infoNeeds".equals(args[i]) && i + 1 < args.length) {
-                infoNeedsFile = args[i + 1];
-            } else if ("-output".equals(args[i]) && i + 1 < args.length) {
-                resultsFile = args[i + 1];
+        // Specify the directory containing RDF files
+        String rdfFilesDir = args[1];
+
+        // Create a TDB dataset
+        Dataset dataset = TDBFactory.createDataset(datasetDir);
+
+        // Iterate over RDF files in the directory
+        File rdfDirectory = new File(rdfFilesDir);
+        File[] rdfFiles = rdfDirectory.listFiles((dir, name) -> name.endsWith(".rdf"));
+
+        if (rdfFiles != null) {
+            for (File rdfFile : rdfFiles) {
+                System.out.println("Indexing RDF file: " + rdfFile.getName());
+
+                // Get the model from the dataset
+                dataset.begin(ReadWrite.WRITE);
+                try {
+                    Model model = dataset.getDefaultModel();
+                    model.read(rdfFile.getAbsolutePath());
+                    dataset.commit();
+                } finally {
+                    dataset.end();
+                }
             }
         }
 
-        // Verificar que se proporcionaron todas las rutas necesarias
-        if (rdfPath == null || infoNeedsFile == null || resultsFile == null) {
-            System.out.println("Se requieren todas las rutas: -rdf <rdfPath> -infoNeeds <infoNeedsFile> -output <resultsFile>");
-            System.exit(1);
+        // Perform SPARQL query
+        String queryString = "SELECT ?subject ?predicate ?object WHERE { ?subject ?predicate ?object }";
+        dataset.begin(ReadWrite.READ);
+        try {
+            Query query = QueryFactory.create(queryString);
+            QueryExecution qexec = QueryExecutionFactory.create(query, dataset);
+            ResultSet results = qexec.execSelect();
+
+            // Process the query results
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                // Access variables in the solution
+                //RDFNode subject = soln.get("subject");
+                //RDFNode predicate = soln.get("predicate");
+                //RDFNode object = soln.get("object");
+
+                // Process the results as needed
+            //    System.out.println(subject + " " + predicate + " " + object);
+            }
+        } finally {
+            dataset.end();
         }
-
-        // Validar la existencia de archivos
-        if (!validarExistenciaArchivos(rdfPath, infoNeedsFile)) {
-            System.out.println("Alguno de los archivos proporcionados no existe.");
-            System.exit(1);
-        }
-
-        // lógica para procesar las necesidades de información desde un archivo XML
-        // realizar busqueda en el grafo RDF para generar los resultados
-        // TODO
-
-        // imprimir ruta del archivo
-        System.out.println("Archivo RDF: " + rdfPath);
-        // imprimir ruta del archivo de necesiadades de informacion
-        System.out.println("Archivo de Necesidades de Información: " + infoNeedsFile);
-        // ruta del archivo de resutlados
-        System.out.println("Archivo de Resultados: " + resultsFile);
-
     }
-    private static boolean validarExistenciaArchivos(String... paths) {
-        for (String path : paths) {
-            File file = new File(path);
-            if (!file.exists()) {
-                return false;
-            }
+
+
+
+
+
+    /**
+     * Función que comprueba que el número de parámetros es correcto
+     */
+    private static void comprobarParametros(int numParametros){
+        if (numParametros != 6) {
+            System.out.println("Número incorrecto de parámetros.\nUso: SemanticSearcher -rdf <rdfPath> -infoNeeds <infoNeedsFile> -output <resultsFile>");
+            System.exit(1);
         }
-        return true;
     }
 }
